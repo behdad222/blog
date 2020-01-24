3d0198ddb0cf28aa0c0a1743558a5a47e29bee43
behdad.222 <behdad.222@gmail.com>
2020-01-24 19:10:38 +0330

    add ci config

---
+++ .circle/setup-github.sh
@@ -0,0 +1,15 @@
+git config user.name "behdad222CI"
+git config user.email "behdad.222+CI@gmail.com"
+
+git checkout gh-pages
+git pull origin gh-pages
+
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv _site/* .
+rm -R _site/
+
+git add -fA
+git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
+git push origin gh-pages
+
+echo "deployed successfully"
